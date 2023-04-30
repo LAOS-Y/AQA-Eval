8229916c739c4964796bf6c2a2075db3e77cb985
LAOS-Y <billyang426@gmail.com>
2023-04-30 21:03:14 +0000

    update chatgpt api

--- models/ChatGPT.py
+++ models/ChatGPT.py
@@ -2,12 +2,17 @@ import openai
 import time
 
 openai.api_key = ""
-openai.api_base = 'https://lunarai.deno.dev/v1'
+openai.api_base = "your_api_base_url"
+
 
 class ChatGPT():
     def __init__(self):
-        self.messages = [{"role": "system", "content": "You are a chatbot"}]
-    
+        self.reset()
+
+    def reset(self, instruction="You are a chatbot"):
+        self.messages = [{"role": "system", "content": instruction}]
+        self.history = []
+
     def __call__(self, prompt):
         time.sleep(20)
         self.messages.append({"role": "user", "content": prompt})
@@ -22,14 +27,14 @@ class ChatGPT():
             result += choice.message.content
 
         self.messages.append({"role": "assistant", "content": result})
+        self.history.append((prompt, result))
 
         return result
-    
-    def reset(self):
-        self.messages = [{"role": "system", "content": "You are a chatbot"}]
-    
-    def teacher_force(self, new_reply):
-        self.messages.pop()
 
-        self.messages.append({"role": "assistant", "content": new_reply})
-        return
+    def revoke(self, n=1):
+        assert 0 <= n and n <= len(self.history)
+        self.history = self.history[:-n]
+
+    def force(self, new_reply):
+        self.messages[-1]["content"] = new_reply
+        self.history[-1] = (self.history[-1][0], new_reply, *self.history[-1][1:])
