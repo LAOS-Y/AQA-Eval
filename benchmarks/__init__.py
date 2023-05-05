206f1e95d5399b40840a669606b5921c0d83eb46
Xyyyy123 <2210354438@qq.com>
2023-05-05 17:56:58 +0800

    BFS: add bfs model

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,2 +1,3 @@
 from .binary_search import BinarySearchEvaluator
 from .dfs import DFSEvaluator
+from .bfs import BfsEvaluator
--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from .chatgpt import ChatGPT
 from .llama import Llama
 from .simple_model import SimpleModel
 from .dfs_model import DFSModel
+from .bfs_model import BFSModel
