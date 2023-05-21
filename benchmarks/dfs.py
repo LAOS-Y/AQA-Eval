3a9dc79fcae2783161581b4f5fa4f6db040dac4f
LAOS-Y <billyang426@gmail.com>
2023-05-21 15:38:20 +0000

    add `test_multi_time`

--- benchmarks/dfs.py
+++ benchmarks/dfs.py
@@ -349,3 +349,93 @@ class DFSEvaluator():
         )
 
         return metric, full_result
+
+    def _pack_multi_time_result(self, metrics, single_results, teacher_forcing_mode):
+        # summarize the metrics from each test run and pack the detailed results
+        metric = dict_mean(metrics)
+
+        metric = {"mean_" + k: v for k, v in metric.items()}
+
+        full_result = {}
+        full_result["metric"] = metric
+        full_result["env"] = dict(
+            # min=self.min,
+            # max=self.max,
+            graph=self._graph,
+            times=len(metrics),
+            teacher_forcing_mode=teacher_forcing_mode,
+            default_insturction=self.default_insturction
+        )
+        full_result["single_results"] = single_results
+
+        return metric, full_result
+
+    def _independent_test(self, model, times, teacher_forcing_mode):
+        teacher_forcing = teacher_forcing_mode == "l1"
+
+        metrics = []
+        single_results = []
+        for i in range(times):
+            metric, single_result = self.test_one_time(model, teacher_forcing)
+
+            logger.info(f"Evaluation metric #{i}: {metric}")
+            metrics.append(metric)
+            single_results.append(single_result)
+
+        return self._pack_multi_time_result(metrics, single_results, teacher_forcing_mode)
+
+    def _context_kept_test(self, model, times, teacher_forcing_mode):
+        # model's history will be cleared before each run
+        # teacher model's history will be used as example in the intruction prompt
+        # at the beginning of each run
+
+        def get_tf_flag(i):
+            # i: current run is the `i`-th run. `i` in [0, `times` - 1]
+            if teacher_forcing_mode == "l2":
+                return False
+
+            if teacher_forcing_mode == "l4":
+                return True
+
+            return i < times - 1
+
+        instruction_w_examples = self.default_insturction \
+            + "\nHere are some examples (the right answer for each example is different):\n"
+
+        metrics = []
+        single_results = []
+        for i in range(times):
+            metric, single_result = self.test_one_time(
+                model, get_tf_flag(i),
+                instruction=None if i == 0 else instruction_w_examples
+            )
+
+            logger.info(f"Evaluation metric #{i}: {metric}")
+
+            if not get_tf_flag(i):
+                self.refresh_teacher_qa()
+                single_result["history"]["teacher_history"] = self._teacher_qa_list
+
+            metrics.append(metric)
+            single_results.append(single_result)
+
+            example_ctx = f"Example #{i + 1}: \n" + model.rebuild_context(self._teacher_qa_list)
+            instruction_w_examples += example_ctx
+
+        return self._pack_multi_time_result(metrics, single_results, teacher_forcing_mode)
+
+    def test_multi_time(self, model, times, teacher_forcing_mode="l0"):
+        # teacher forcing options:
+        # "l0": no teacher forcing, context is cleared after each test
+        # "l1": naive teacher forcing, context is cleared after each test
+        # "l2": no teacher forcing during the current test, previous context is used as
+        #       initial prompt after forced
+        # "l3": similar to "l4" but the final test runs in the "l2" mode
+        # "l4": full teacher forcing, previous context is used as initial prompt after forced
+
+        assert teacher_forcing_mode in ["l0", "l1", "l2", "l3", "l4"], teacher_forcing_mode
+
+        if teacher_forcing_mode in ["l0", "l1"]:
+            return self._independent_test(model, times, teacher_forcing_mode)
+
+        return self._context_kept_test(model, times, teacher_forcing_mode)
