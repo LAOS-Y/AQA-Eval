0b0b63e8b2de3854503d833ee6478e756ffff24c
LAOS-Y <billyang426@gmail.com>
2023-09-11 21:45:46 +0000

    change some api

--- benchmarks/dfs.py
+++ benchmarks/dfs.py
@@ -3,18 +3,18 @@ import re
 
 from loguru import logger
 
-from models.dfs_model import DFSModel
+from models import DFSModel
 from utils import DialogLogger, Invalid, FormatInvalid, ValueInvalid, dict_mean
 
 
 class DFSEvaluator():
     def __init__(
-        self, node_num=10, explain_algo=True, mcq=True, provide_state=True,
+        self, node_num=4, explain_algo=True, mcq=True, provide_state=True,
         format_tolerant=True, max_retry=0, max_step=20
     ):
         self.node_num = node_num
-        self.explain_algo = explain_algo
         self.mcq = mcq
+        self.explain_algo = explain_algo
         self.provide_state = provide_state
         self.format_tolerant = format_tolerant
         # `max_retry` and `max_step` are only activated when not teacher forcing
@@ -24,10 +24,11 @@ class DFSEvaluator():
         self.dialog_logger = DialogLogger(order=["System", "Q", "A", "T"])
 
     def reset(self):
+        self._teacher_qa_list = []
         self.teacher.reset("")
-        self._teacher_qa_list = None
-        self._graph = None
-        self._start_node = None
+        # self._teacher_qa_list = None
+        # self._graph = None
+        # self._start_node = None
 
     @property
     def default_insturction(self):
@@ -61,35 +62,34 @@ class DFSEvaluator():
     def _get_adj_nodes(self, curr_node):
         return [n for _, n in self._graph.edges(curr_node)]
 
-    def _get_prompt(self, next_node, node_history):
+    def _get_prompt(self, next_node, visited_nodes):
         '''
         Generate prompt used in exploration step
 
         Return: prompt (string)
         '''
 
-        if len(set(node_history + [next_node])) == len(self._graph.nodes):
+        if len(set(visited_nodes + [next_node])) == len(self._graph.nodes):
             return "Well Done. You have visited all the nodes in the graph. " \
-                   "Total number of steps: {}".format(len(node_history[1:] + [next_node]))
+                   "Total number of steps: {}".format(len(visited_nodes[1:] + [next_node]))
 
         adj_nodes = self._get_adj_nodes(next_node)
 
         prompt = "Adjacent nodes: {}.".format(", ".join([str(i) for i in adj_nodes]))
 
         if self.provide_state:
-            unvisited_adj_nodes = set(adj_nodes).difference(set(node_history))
+            unvisited_adj_nodes = set(adj_nodes).difference(set(visited_nodes))
             if len(unvisited_adj_nodes) == 0:
                 prompt += " You have visited all nodes adjacent to this node."
             else:
                 prompt += " You have not visited node {}." \
                           .format(", ".join([str(i) for i in unvisited_adj_nodes]))
         if self.mcq:
-            prompt += " Choose the next node to visit: {}." \
-                      .format(", ".join([str(i) for i in adj_nodes]))
+            prompt += " Choose the next node to visit: {}.".format(", ".join(adj_nodes))
 
         return prompt
 
-    def extract_answer(self, reply, adj_nodes):
+    def extract_answer(self, reply, valid_nodes):
         # parse reply from model and return the formated answer
         # return an `Invalid` if failed to do so
         if self.format_tolerant:
@@ -99,14 +99,14 @@ class DFSEvaluator():
 
             next_node = int(nums[0])
 
-            if next_node not in adj_nodes:
+            if next_node not in valid_nodes:
                 return ValueInvalid(next_node)
             return next_node
 
         try:
             next_node = int(reply)
 
-            if next_node not in adj_nodes:
+            if next_node not in valid_nodes:
                 return ValueInvalid(next_node)
             return next_node
         except ValueError:
@@ -142,8 +142,9 @@ class DFSEvaluator():
 
         return if_dfs
 
-    def calc_decoverage(self, next_node, node_history):
-        return 1 - len(set(node_history + [next_node])) / len(self._graph.nodes)
+    def calc_decoverage(self, visited_nodes):
+        assert self._start_node in visited_nodes
+        return 1 - len(set(visited_nodes)) / len(self._graph.nodes)
 
     def refresh_teacher_qa(self):
         self.reset_model(self.teacher, verbose=False)
@@ -151,7 +152,7 @@ class DFSEvaluator():
 
         response = ""
         prompt = self._get_prompt(self._start_node, [])
-        decov_sum = self.calc_decoverage(self._start_node, [])
+        decov_sum = self.calc_decoverage([self._start_node])
 
         next_node = self._start_node
         node_history = [self._start_node]
@@ -164,7 +165,7 @@ class DFSEvaluator():
             self._teacher_qa_list.append((prompt, next_node))
             prompt = self._get_prompt(next_node, node_history)
 
-            decov_sum += self.calc_decoverage(next_node, node_history)
+            decov_sum += self.calc_decoverage(node_history + [next_node])
             node_history.append(next_node)
 
         self._teacher_qa_list.append((prompt, None))
@@ -175,7 +176,7 @@ class DFSEvaluator():
         # TODO: remove the starting node in all `node_history`
         assert len(node_history) > 1
 
-        decov_list = [self.calc_decoverage(self._start_node, [])]
+        decov_list = [self.calc_decoverage([self._start_node])]
         highest_cnt = 0
         check_dfs_flag = True
 
@@ -191,21 +192,21 @@ class DFSEvaluator():
             else:
                 check_dfs_flag = False
 
-            decov = self.calc_decoverage(node, node_history[:idx + 1])
+            decov = self.calc_decoverage(node_history[:idx + 1] + [node])
             assert decov <= decov_list[-1], "`decov_list` should be a non-ascent sequence"
             decov_list.append(decov)
 
-        acc = highest_cnt / len(node_history[1:])  # ignore the starting node
+        acc = highest_cnt / len(node_history[1:])  # ignore the starting node  # dont ignore last invalid
         min_decov = decov_list[-1]
         sum_decov = sum(decov_list)
 
-        metrics = {"acc": acc, "min_decov": min_decov, "sum_decov": sum_decov}
+        metrics = {"acc": acc, "min_decov": min_decov, "sum_decov": sum_decov, "decov_list": decov_list}
         return metrics
 
     def calc_metric_tf(self, node_history, teacher_node_history):
         assert len(node_history) > 1
 
-        decov_list = [self.calc_decoverage(self._start_node, [])]
+        decov_list = [self.calc_decoverage([self._start_node])]
         dfs_cnt = 0
 
         for idx, node in enumerate(node_history[1:]):
@@ -216,7 +217,7 @@ class DFSEvaluator():
             if self._check_dfs(node, teacher_node_history[:idx + 1]):
                 dfs_cnt += 1
 
-            decov = self.calc_decoverage(node, teacher_node_history[:idx + 1])
+            decov = self.calc_decoverage(teacher_node_history[:idx + 1] + [node])
             assert decov <= decov_list[-1], "`decov_list` should be a non-ascent sequence"
             decov_list.append(decov)
 
@@ -239,6 +240,8 @@ class DFSEvaluator():
 
         retry_cnt = 0
 
+        value_valid_nodes = set(self._get_adj_nodes(self._start_node))
+
         while (
             len(set(node_history)) != len(self._graph.nodes) and
             (len(node_history) - 1) < self.max_step and retry_cnt < (self.max_retry + 1)
@@ -249,7 +252,7 @@ class DFSEvaluator():
             self.dialog_logger.info(A=reply)
 
             # start processing response in this iteration
-            next_node = self.extract_answer(reply, self._get_adj_nodes(node_history[-1]))
+            next_node = self.extract_answer(reply, value_valid_nodes)
 
             # if `reply` is formatted, force the new reply
             if not isinstance(next_node, FormatInvalid) \
@@ -263,12 +266,15 @@ class DFSEvaluator():
                 prompt = self._get_prompt(next_node, node_history)
                 node_history.append(next_node)
                 retry_cnt = 0
+
+                value_valid_nodes = set(self._get_adj_nodes(next_node))
                 continue
 
             if retry_cnt == 0:
                 # TODO: maybe add mcq here?
                 prompt = "Invalid reply. Try again. You can only reply with a " \
-                         "integer number. " + prompt
+                         "integer number."
+
             retry_cnt += 1
 
         self.dialog_logger.info(Q=prompt)
@@ -282,7 +288,7 @@ class DFSEvaluator():
         return node_history
 
     def _test_tf(self, model):
-        curr_node = self._start_node
+        value_valid_nodes = self._get_adj_nodes(self._start_node)
         node_history = [self._start_node]
         teacher_node_history = [self._start_node]
 
@@ -297,11 +303,11 @@ class DFSEvaluator():
             model.force(str(teacher_reply))
             self.dialog_logger.info(A=reply, T=teacher_reply)
 
-            next_node = self.extract_answer(reply, self._get_adj_nodes(curr_node))
+            next_node = self.extract_answer(reply, value_valid_nodes)
 
             node_history.append(next_node)
             teacher_node_history.append(teacher_reply)
-            curr_node = teacher_reply
+            value_valid_nodes = self._get_adj_nodes(teacher_reply)
 
         self.dialog_logger.info(Q=self._teacher_qa_list[-1][0])
 
