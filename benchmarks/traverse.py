4d41bddae22d3c43e038efcd92eb964366a27088
LAOS-Y <billyang426@gmail.com>
2023-09-14 22:22:21 +0000

    TraverseGraphEvaluator inherets Benchmark

--- benchmarks/traverse.py
+++ benchmarks/traverse.py
@@ -3,45 +3,26 @@ import re
 
 from loguru import logger
 
-from utils import DialogLogger, Invalid, FormatInvalid, ValueInvalid, dict_mean
+from utils import Invalid, FormatInvalid, ValueInvalid
+from .benchmark import Benchmark
 
 
 # TODO: refine or just remove mcq and provide_state
-class TraverseGraphEvaluator():
+class TraverseGraphEvaluator(Benchmark):
     def __init__(
         self, node_num=4, explain_algo=True, mcq=False, provide_state=False,
         format_tolerant=True, max_retry=0, max_step=None
     ):
+        super(TraverseGraphEvaluator, self).__init__(format_tolerant, max_retry, max_step)
         self.node_num = node_num
-        self.mcq = mcq
         self.explain_algo = explain_algo
+        self.mcq = mcq
         self.provide_state = provide_state
-        self.format_tolerant = format_tolerant
-        # `max_retry` and `max_step` are only activated when not teacher forcing
-        self.max_retry = max_retry
-        self.max_step = max_step
-        self.dialog_logger = DialogLogger(order=["System", "Q", "A", "T"])
 
         # Set `self.teacher` here
 
     def reset(self):
-        self._teacher_qa_list = []
-        self.teacher.reset("")
-
-    @property
-    def default_insturction(self):
-        raise NotImplementedError
-
-    def reset_model(self, model, instruction=None, verbose=True):
-        # clear dialog history and give instruction
-        # will use `self.default_insturction` if `instruction` is None
-        if instruction is None:
-            instruction = self.default_insturction
-
-        if verbose:
-            self.dialog_logger.info(System=instruction)
-
-        model.reset(instruction)
+        super(TraverseGraphEvaluator, self).reset()
 
     def _get_adj_nodes(self, curr_node):
         return [n for _, n in self._graph.edges(curr_node)]
@@ -78,8 +59,8 @@ class TraverseGraphEvaluator():
 
         return prompt
 
-    def extract_answer(self, reply, valid_nodes):
-        # parse reply from model and return the formated answer
+    def _extract_answer(self, reply, valid_nodes):
+        # parse reply from model and return the formatted answer
         # return an `Invalid` if failed to do so
         if self.format_tolerant:
             nums = re.findall(r'\d+', reply)
@@ -122,8 +103,7 @@ class TraverseGraphEvaluator():
         return 1 - len(set(visited_nodes)) / len(self._graph.nodes)
 
     def refresh_teacher_qa(self):
-        self.reset_model(self.teacher, verbose=False)
-        self._teacher_qa_list = []
+        super(TraverseGraphEvaluator, self).refresh_teacher_qa()
 
         response = ""
         prompt = self._get_prompt(self._start_node, [])
@@ -189,10 +169,13 @@ class TraverseGraphEvaluator():
         decov_list = decov_list[1:]
         sum_decov = sum(decov_list)
 
-        metrics = {
-            "acc": acc, "min_decov": min_decov, "sum_decov": sum_decov, "decov_list": decov_list
+        metric = {
+            "acc": acc,
+            "min_decov": min_decov,
+            "sum_decov": sum_decov,
+            # "decov_list": decov_list
         }
-        return metrics
+        return metric
 
     def calc_metric_tf(self, node_history, teacher_node_history):
         assert len(node_history) > 0
@@ -234,10 +217,13 @@ class TraverseGraphEvaluator():
         decov_list = decov_list[1:]
         sum_decov = sum(decov_list)
 
-        metrics = {
-            "acc": acc, "min_decov": min_decov, "sum_decov": sum_decov, "decov_list": decov_list
+        metric = {
+            "acc": acc,
+            "min_decov": min_decov,
+            "sum_decov": sum_decov,
+            # "decov_list": decov_list
         }
-        return metrics
+        return metric
 
     def _test_no_tf(self, model):
         '''
@@ -264,7 +250,7 @@ class TraverseGraphEvaluator():
             self.dialog_logger.info(A=reply)
 
             # start processing response in this iteration
-            next_node = self.extract_answer(reply, valid_nodes)
+            next_node = self._extract_answer(reply, valid_nodes)
 
             # if `reply` is formatted, force the new reply
             if not isinstance(next_node, FormatInvalid) \
@@ -316,7 +302,7 @@ class TraverseGraphEvaluator():
             model.force(str(teacher_reply))
             self.dialog_logger.info(A=reply, T=teacher_reply)
 
-            next_node = self.extract_answer(reply, valid_nodes)
+            next_node = self._extract_answer(reply, valid_nodes)
 
             node_history.append(next_node)
             teacher_node_history.append(teacher_reply)
@@ -343,117 +329,46 @@ class TraverseGraphEvaluator():
             model_node_history, teacher_node_history, optim_decov_sum = self._test_tf(model)
             metric = self.calc_metric_tf(model_node_history, teacher_node_history)
         else:
+            teacher_node_history = []
             model_node_history = self._test_no_tf(model)
             metric = self.calc_metric_no_tf(model_node_history)
 
-        full_result = {}
-        full_result["metric"] = metric
-        full_result["output"] = dict(
-            node_history=model_node_history,
-            teacher_node_history=teacher_node_history if teacher_forcing else None
+        result = self._get_result(
+            metric, model_node_history, teacher_node_history,
+            model.history, teacher_forcing, instruction
         )
-        full_result["env"] = dict(
-            optim_decov_sum=optim_decov_sum if teacher_forcing else None,
+
+        return metric, result
+
+    # TODO: maybe `teacher_answer_list`, `_teacher_qa_list` should be None if no `teacher_forcing`
+    def _get_result(
+        self, metric, answer_list, teacher_answer_list,
+        model_history, teacher_forcing, instruction=None
+    ):
+        result = super(TraverseGraphEvaluator, self)._get_result(
+            metric, answer_list, teacher_answer_list, model_history, teacher_forcing, instruction
+        )
+
+        result["env"].update(dict(
+            # optim_decov_sum=optim_decov_sum if teacher_forcing else None,
             nodes=list(self._graph.nodes),
             edges=list(self._graph.edges),
             start_node=self._start_node,
-            teacher_forcing=teacher_forcing,
             mcq=self.mcq,
             explain_algo=self.explain_algo,
             provide_state=self.provide_state,
-            instruction=self.default_insturction if instruction is None else instruction
+        ))
+
+        return result
+
+    def _pack_results(self, metrics, single_results, teacher_forcing_mode):
+        metrics, full_result = super(TraverseGraphEvaluator, self)._pack_results(
+            metrics, single_results, teacher_forcing_mode
         )
-        full_result["history"] = dict(
-            model_history=model.history,
-            teacher_history=self._teacher_qa_list if teacher_forcing else None
-        )
 
-        return metric, full_result
-
-    def _pack_multi_time_result(self, metrics, single_results, teacher_forcing_mode):
-        # summarize the metrics from each test run and pack the detailed results
-        metric = dict_mean(metrics)
-
-        metric = {"mean_" + k: v for k, v in metric.items()}
-
-        full_result = {}
-        full_result["metric"] = metric
-        full_result["env"] = dict(
+        full_result["env"].update(dict(
             graph=self._graph,
-            times=len(metrics),
-            teacher_forcing_mode=teacher_forcing_mode,
-            default_insturction=self.default_insturction
-        )
-        full_result["single_results"] = single_results
+            start_node=self._start_node,
+        ))
 
-        return metric, full_result
-
-    def _independent_test(self, model, times, teacher_forcing_mode):
-        teacher_forcing = teacher_forcing_mode == "l1"
-
-        metrics = []
-        single_results = []
-        for i in range(times):
-            metric, single_result = self.test_one_time(model, teacher_forcing)
-
-            logger.info(f"Evaluation metric #{i}: {metric}")
-            metrics.append(metric)
-            single_results.append(single_result)
-
-        return self._pack_multi_time_result(metrics, single_results, teacher_forcing_mode)
-
-    def _context_kept_test(self, model, times, teacher_forcing_mode):
-        # model's history will be cleared before each run
-        # teacher model's history will be used as example in the intruction prompt
-        # at the beginning of each run
-
-        def get_tf_flag(i):
-            # i: current run is the `i`-th run. `i` in [0, `times` - 1]
-            if teacher_forcing_mode == "l2":
-                return False
-
-            if teacher_forcing_mode == "l4":
-                return True
-
-            return i < times - 1
-
-        instruction_w_examples = self.default_insturction \
-            + "\nHere are some examples (the right answer for each example is different):\n"
-
-        metrics = []
-        single_results = []
-        for i in range(times):
-            metric, single_result = self.test_one_time(
-                model, get_tf_flag(i),
-                instruction=None if i == 0 else instruction_w_examples
-            )
-
-            logger.info(f"Evaluation metric #{i}: {metric}")
-
-            if not get_tf_flag(i):
-                self.refresh_teacher_qa()
-                single_result["history"]["teacher_history"] = self._teacher_qa_list
-
-            metrics.append(metric)
-            single_results.append(single_result)
-
-            example_ctx = f"Example #{i + 1}: \n" + model.rebuild_context(self._teacher_qa_list)
-            instruction_w_examples += example_ctx
-
-        return self._pack_multi_time_result(metrics, single_results, teacher_forcing_mode)
-
-    def test_multi_time(self, model, times, teacher_forcing_mode="l0"):
-        # teacher forcing options:
-        # "l0": no teacher forcing, context is cleared after each test
-        # "l1": naive teacher forcing, context is cleared after each test
-        # "l2": no teacher forcing during the current test, previous context is used as
-        #       initial prompt after forced
-        # "l3": similar to "l4" but the final test runs in the "l2" mode
-        # "l4": full teacher forcing, previous context is used as initial prompt after forced
-
-        assert teacher_forcing_mode in ["l0", "l1", "l2", "l3", "l4"], teacher_forcing_mode
-
-        if teacher_forcing_mode in ["l0", "l1"]:
-            return self._independent_test(model, times, teacher_forcing_mode)
-
-        return self._context_kept_test(model, times, teacher_forcing_mode)
+        return metrics, full_result
