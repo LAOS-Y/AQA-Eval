8226c01039ca673085aa8151e0bb8fe27c90a0ce
LAOS-Y <billyang426@gmail.com>
2023-12-11 23:04:21 +0000

    fix fastchat config names

--- aqa/configs/models/fastchat_model.py
+++ aqa/configs/models/fastchat_model.py
@@ -42,10 +42,10 @@ LLAMA2_13B_CONFIG.update(MODEL_PATH="meta-llama/Llama-2-13b-hf")
 LLAMA2_13B_CHAT_CONFIG = deepcopy(FASTCHAT_MODEL_CONFIG)
 LLAMA2_13B_CHAT_CONFIG.update(MODEL_PATH="meta-llama/Llama-2-13b-chat")
 
-LLAMA2_70B = deepcopy(FASTCHAT_MODEL_CONFIG)
-LLAMA2_70B.update(MODEL_PATH="meta-llama/Llama-2-70b-hf")
+LLAMA2_70B_CONFIG = deepcopy(FASTCHAT_MODEL_CONFIG)
+LLAMA2_70B_CONFIG.update(MODEL_PATH="meta-llama/Llama-2-70b-hf")
 
-LLAMA2_70B_CHAT = deepcopy(FASTCHAT_MODEL_CONFIG)
-LLAMA2_70B_CHAT.update(MODEL_PATH="meta-llama/Llama-2-70b-chat")
+LLAMA2_70B_CHAT_CONFIG = deepcopy(FASTCHAT_MODEL_CONFIG)
+LLAMA2_70B_CHAT_CONFIG.update(MODEL_PATH="meta-llama/Llama-2-70b-chat")
 
 __all__ = [k for k in globals().keys() if "_CONFIG" in k]
