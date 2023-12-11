c08e4229607167961bcead60e5f09a7febb1d83c
LAOS-Y <billyang426@gmail.com>
2023-12-11 13:53:37 +0000

    add eval

--- aqa/utils/__init__.py
+++ aqa/utils/__init__.py
@@ -1,6 +1,7 @@
 from .config import Config
 from .dict_boardcast import dict_mean, dict_sum, dict_max, dict_min
 from .dialog_logger import DialogLogger
+from .eval import eval
 from .file import ensure_dir
 from .invalid import Invalid, InvalidEncoder, FormatInvalid, ValueInvalid
 from .logger import setup_logger
---
+++ aqa/utils/eval.py
@@ -0,0 +1,21 @@
+from aqa.benchmarks import build_benchmark
+from aqa.models import build_model
+
+
+def eval(config):
+    benchmark = build_benchmark(config)
+    model = build_model(config)
+
+    times = config.EVAL.TIMES
+    num_exp = config.EVAL.NUM_EXAMPLES
+    tf = config.EVAL.TEACHER_FORCING
+    resume = config.EVAL.RESUME
+
+    metric, full_result = benchmark.test_with_examples(
+        model, times,
+        num_examples=num_exp,
+        teacher_forcing=tf,
+        resume=resume
+    )
+
+    return metric, full_result
