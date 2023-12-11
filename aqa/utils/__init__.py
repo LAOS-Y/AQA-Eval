3ce4920855095de015c71ad7f6be90d772c58d94
LAOS-Y <billyang426@gmail.com>
2023-12-11 22:48:00 +0000

    add dynamic_import and new entrypoint

--- aqa/utils/__init__.py
+++ aqa/utils/__init__.py
@@ -1,5 +1,6 @@
 from .dict_boardcast import dict_mean, dict_sum, dict_max, dict_min
 from .dialog_logger import DialogLogger
+from .dynamic_import import dynamic_import
 from .eval import eval
 from .file import ensure_dir
 from .invalid import Invalid, InvalidEncoder, FormatInvalid, ValueInvalid
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import json
-import rich
+import argparse
 from loguru import logger
 
-import models
-from benchmarks import BinarySearchEvaluator
+from aqa.utils import dynamic_import, eval
 
-evaluator = BinarySearchEvaluator(0, 2**10, format_tolerant=True, max_retry=3, max_guess=15)
-model = models.Llama(
-    "/data3/siwei/my_llama/tokenizer/",
-    "/data3/siwei/my_llama/vicuna/vicuna-7b"
-)
-logger.info("Start testing Llama model with teacher forcing.")
-err, full_result = evaluator.test_multi_time(model, 2, teacher_forcing_mode="l2")
-rich.print("Full result:", full_result)
-rich.print("Err of Llama:", err)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-c", type=str, required=True)
 
-json.dump(full_result, open("vicuna-7b-l2.json", mode="w"))
+    args = parser.parse_args()
+
+    config = dynamic_import(args.config).config
+    logger.info(config)
+    eval(config)
