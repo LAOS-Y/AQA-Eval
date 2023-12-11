df5bdbb603dc8ac6238ccca5f3041c7d6996fa6d
LAOS-Y <billyang426@gmail.com>
2023-12-11 23:04:47 +0000

    add bs exp3 configs with llama2 and vicuna

---
+++ configs/bs/llama2_13b/exp3.py
@@ -0,0 +1,17 @@
+from copy import deepcopy
+
+from aqa.configs import benchmarks, models
+from aqa.configs.base_config import BASE_CONFIG
+
+config = deepcopy(BASE_CONFIG)
+config.update(
+    BENCHMARK=dict(
+        benchmarks.BINARY_SEARCH_CONFIG,
+        DATASET_FILE="binary_search_0.json",
+        OUTPUT_DIR=__file__.replace("configs", "results", 1)
+    ),
+    MODEL=models.LLAMA2_13B_CONFIG,
+    EVAL=dict(
+        NUM_EXAMPLES=3,
+    )
+)
