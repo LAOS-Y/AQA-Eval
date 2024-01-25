91b32111c9fcdfb7202af23788a1686be63f2470
LAOS-Y <billyang426@gmail.com>
2024-01-25 00:51:29 +0000

    add three embodied benchmarks

--- aqa/configs/benchmarks/__init__.py
+++ aqa/configs/benchmarks/__init__.py
@@ -1,3 +1,5 @@
 from .benchmark import BENCHMARK_BASE_CONFIG
 from .binary_search import BINARY_SEARCH_CONFIG
+from .cave import CAVE_BFS_CONFIG, CAVE_DFS_CONFIG
+from .coin import COIN_CONFIG
 from .traverse import BFS_CONFIG, DFS_CONFIG
